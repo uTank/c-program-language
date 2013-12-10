a056499b90b4c73a499011a6a39a15fe33b6f5b9
uTank <hongyt2045@163.com>
2013-12-10 17:54:27 +0800

    Create add.c

---
+++ add.c
@@ -0,0 +1,15 @@
+
+// TODO: 0
+int add(int x, int y)
+{
+     int a, b;
+     do{
+          a = x & y;
+          b = x ^ y;
+          x = a << 1;
+          y = b;
+     while(a != 0);
+     return b;
+}
+
+/* */
